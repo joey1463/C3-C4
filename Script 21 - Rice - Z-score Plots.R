ee59aacb8e71e4ea89ccdb65558be3605ec25b53
joey1463 <142527339+joey1463@users.noreply.github.com>
2024-07-19 13:59:32 -0700

    Create Script 21 - Rice - Z-score Plots.R

---
+++ Script 21 - Rice - Z-score Plots.R
@@ -0,0 +1,58 @@
+# This script will take all genes found differentially expressed in response to light in each cell type, and cluster their expression patterns
+# to find dominant gene expression trends.
+# These trends are then visualized using a Z-score plot. 
+
+setwd("~/Desktop/Salk/Project - Light/R Files/")
+chosen_cluster<-c("L3_rice_mesophyll","L3_rice_bundle_sheath","L3_rice_guard","L3_rice_phloem","L3_rice_epidermis","L3_rice_xylem")
+
+DE_genes<-list(NA)
+for (i in 1:length(chosen_cluster)){
+load(paste(chosen_cluster[i],"_DE_Significant.RData",sep=''))
+DE_Significant<-subset(DE_Significant, DE_Significant$padj<2 & DE_Significant$log2FoldChange>0) # change to "<0" for downregualted genes 
+DE_genes[[i]]<-rownames(DE_Significant)}
+
+all_genes<-unique(c(DE_genes[[1]],DE_genes[[2]],DE_genes[[3]],DE_genes[[4]],DE_genes[[5]],DE_genes[[6]]))
+test_genes<-subset(time_average, rownames(time_average) %in% all_genes)
+
+plot(hclust(as.dist(1-cor(t(test_genes))), "ave"))
+clustering_1<-hclust(as.dist(1-cor(t(test_genes))), "ave")
+table(cutree(clustering_1, h=0.65))
+cut_groups<-cutree(clustering_1, h=0.65) 
+table(cut_groups)
+order<-order(table(cut_groups), decreasing=TRUE)
+
+cluster_groups<-list(NA)
+for (i in 1:length(unique(cut_groups))) {groupings<-subset(test_genes, rownames(test_genes) %in% names(cut_groups[cut_groups==i]))
+                                         cluster_groups[[i]]<-as.data.frame(groupings)
+                                         cluster_groups[[i]][,length(test_genes[1,])+1]<-c(rep(i,length(cluster_groups[[i]][,1])))}
+
+plot_list<-list(NA)
+for (i in 1:length(order)) {
+cluster_group<-cluster_groups[[i]][,c(1:length(test_genes[1,]))]
+Z <- as.data.frame(t(scale(t(cluster_group))))
+Z_average<-colSums(Z)/length(Z[,1])
+Z<-rbind(Z, Z_average)
+rownames(Z)[length(Z[,1])]<-"average"
+plot_matrix<-melt(t(Z))
+factored<-as.character(plot_matrix[,1])
+time<-substr(factored,1,3)
+time<-str_remove(time, "L") 
+time<-as.numeric(str_remove(time, "_"))
+cell_type<-substr(factored,11,25)
+cell_type<-str_remove(cell_type, "e_") 
+cell_type<-str_remove(cell_type, "_") 
+cell_type<-str_replace(cell_type, "bundlsheath","bundlesheath") 
+plot_matrix[,4]<-time
+plot_matrix[,5]<-paste(cell_type,plot_matrix[,2])
+plot_matrix[,1]<-cell_type
+plot_matrix[,6]<-as.numeric(plot_matrix[,2] == 'average')
+colnames(plot_matrix)<-c("cell_type","gene","expression","time","gene_tissue","color")
+plot_matrix<-subset(plot_matrix, plot_matrix$gene %in% c('average'))
+title<-paste(dim(cluster_group)[1],"genes",sep=" ")
+plot_list[[i]]<-ggplot(data=plot_matrix, aes(x=time, y=expression, group=gene_tissue)) + geom_smooth(method = "loess", se = FALSE, aes(color=cell_type)) +   scale_x_continuous(breaks=c(0,4,8,12)) + theme_bw()  + xlab("time (h)") + ylab("Z-score") +labs(title=title) + theme(legend.position = "none") }  #
+
+# Figure 3
+grid.arrange(plot_list[[order[1]]],plot_list[[order[6]]],plot_list[[order[5]]],plot_list[[order[2]]],plot_list[[order[3]]],plot_list[[order[4]]],nrow=2) 
+
+# Supplementary Figure
+grid.arrange(plot_list[[order[1]]],plot_list[[order[2]]],plot_list[[order[3]]],plot_list[[order[6]]],plot_list[[order[5]]],plot_list[[order[4]]],nrow=2) 
